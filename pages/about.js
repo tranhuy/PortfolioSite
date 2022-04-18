c05a191165e13a38bb335ca480ab3ac08dde9023
Huy <huy.tran@hotmail.com>
2022-04-18 15:44:49 -0400

    uploaded updated resume

--- pages/about.js
+++ pages/about.js
@@ -26,7 +26,7 @@ const About = () => {
                     </div>
                               
                     <div className='flex flex-wrap justify-center gap-2'>               
-                        <a href='/Huy_Dev_CV.pdf' download>
+                        <a href='/Huy_Developer_Resume.pdf' download>
                             <button type='button' className='defaultButton'>
                                 <svg className='w-5 h-5 mr-2 -ml-1' fill='currentColor' viewBox='0 0 122.88 120.89' xmlns='http://www.w3.org/2000/svg'><path d='M84.58,47a7.71,7.71,0,1,1,10.8,11L66.09,86.88a7.72,7.72,0,0,1-10.82,0L26.4,58.37a7.71,7.71,0,1,1,10.81-11L53.1,63.12l.16-55.47a7.72,7.72,0,0,1,15.43.13l-.15,55L84.58,47ZM0,113.48.1,83.3a7.72,7.72,0,1,1,15.43.14l-.07,22q46,.09,91.91,0l.07-22.12a7.72,7.72,0,1,1,15.44.14l-.1,30h-.09a7.71,7.71,0,0,1-7.64,7.36q-53.73.1-107.38,0A7.7,7.7,0,0,1,0,113.48Z'></path></svg>
                                 Dowload CV
